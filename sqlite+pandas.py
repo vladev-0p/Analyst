776e11ede88107772083b827dff2d349e762eaeb
Vlad <dsadio@rambler.ru>
2024-07-27 02:08:30 +0300

    +DataFrame to SQL

--- sqlite+pandas.py
+++ sqlite+pandas.py
@@ -1,6 +1,6 @@
 import sqlite3 as sq3
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 import matplotlib.pyplot as plt
 conn = sq3.connect('work_with_pandas.db')
 
@@ -16,17 +16,21 @@ CREATE TABLE IF NOT EXISTS users(
 ''')
 
 conn.commit()
-users = [
-
-    ('Daniel', 25000),
-
-]
-cur.executemany('''
-INSERT INTO users (name,salary)
-VALUES (?,?)
-''',users)
+# users = [
+#
+#     ('Daniel', 25000),
+#
+# ]
+# cur.executemany('''
+# INSERT INTO users (name,salary)
+# VALUES (?,?)
+# ''',users)
 conn.commit()
 engine= create_engine('sqlite:///work_with_pandas.db')
+'''Adding DataFrame to SQL'''
+df = pd.DataFrame({'name':['Tucker'],
+                    'salary':[85000]})
+df.to_sql(name='users',con=engine ,index_label='id' ,if_exists='append',index=False)
 '''Checking last 3 rows in db.users'''
 table_user = pd.read_sql_table('users', engine)
 conn.close()
